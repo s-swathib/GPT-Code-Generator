61bd53b0140e57a398f6e1cdbb8fe0e3df4913a0
s-swathib <105411200+s-swathib@users.noreply.github.com>
2023-06-08 13:15:16 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,11 @@ from typing import Any, Dict, List
 import openai
 from openai.error import AuthenticationError
 
+openai.api_type = "azure"
+openai.api_base = "https://openaipractice.openai.azure.com/"
+openai.api_version = "2022-12-01"
+openai.api_key = "4587162fb26b4f1185f0fa46ebec945d"
+
 # @st.cache_data
 def get_answer(engine: str, 
                prompt: str, 
