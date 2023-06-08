46dd2d918c8ce5c6c7c802456a75956c6e32c84e
s-swathib <105411200+s-swathib@users.noreply.github.com>
2023-06-08 11:27:16 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -7,7 +7,7 @@ from openai.error import AuthenticationError
 
 # @st.cache_data
 def get_answer(engine: str, 
-               prompt_text: str, 
+               prompt: str, 
                temperature: float, 
                max_tokens: int
               ) -> Dict[str, Any]:
@@ -16,7 +16,7 @@ def get_answer(engine: str,
     # Get the answer
     
     if (engine in ["gpt-35-turbo", "code-davinci-002"]) :
-        response = openai.Completion.create(engine=engine, prompt=prompt_text,temperature=temperature, max_tokens=max_tokens)
+        response = openai.Completion.create(engine=engine, prompt=prompt,temperature=temperature, max_tokens=max_tokens)
 
 
     return response
